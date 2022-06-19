ed3a01145b4323de78f8b1e96b4b2a22d40b0dea
fhb93 <felipe.h.bezerra@gmail.com>
2022-06-19 15:09:35 -0300

    Adicionando mais SFX

---
+++ Assets/Scripts/SFXManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] clips = new AudioClip[5];
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.spatialBlend = 0.6f;
+
+        audioSource.dopplerLevel = 0f;
+
+        audioSource.volume = 0.125f;
+
+        audioSource.loop = false;
+
+        audioSource.clip = clips[Random.Range(0, clips.Length)];
+
+        StartCoroutine(SFXPlayer());
+    }
+
+    IEnumerator SFXPlayer()
+    {
+        while(Application.isPlaying)
+        {
+            audioSource.Play();
+
+            yield return new WaitForSecondsRealtime(audioSource.clip.length);
+
+            audioSource.clip = clips[Random.Range(0, clips.Length)];
+
+            float delay = Random.Range(0.5f, 11f) * 2f;
+
+            yield return new WaitForSecondsRealtime(delay);
+        }
+    }
+}
